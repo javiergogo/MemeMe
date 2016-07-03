6272973d235dd0a76c7f8c27f481f2a974498b57
JAVI GOMEZ <javier.go.go@hotmail.com>
2016-07-03 07:30:42 -0700

    .

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Javier Gomez on 7/2/16.
+//  Copyright © 2016 Javier Gomez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let textTop:String
+    let textBottom:String
+    let imageOriginal : UIImage
+    let imageMemed : UIImage
+}
